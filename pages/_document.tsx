2837d316c5cff8f281cea0e97059b27d4847694d
pablo <pabarragan94@gmail.com>
2022-10-15 21:37:36 +0200

    adsense test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,19 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <Script
+            id="Adsense-id"
+            async
+            strategy="afterInteractive"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1122307556063328"
+            crossOrigin="anonymous"
+           />
+        </Head>
         <body className="">
           <Main />
           <NextScript />
