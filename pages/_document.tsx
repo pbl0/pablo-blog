2f3fdf152730d7cc2aa0c1b4474ba983705a499f
pablo <pabarragan94@gmail.com>
2022-10-20 21:17:09 +0200

    fix ads

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <Script
-            id="Adsense-id"
-            async
-            strategy="afterInteractive"
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1122307556063328"
-            crossOrigin="anonymous"
-           />
+        <script 
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1122307556063328"
+          crossOrigin="anonymous">
+        </script>
         </Head>
         <body className="">
           <Main />
