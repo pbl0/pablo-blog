8fdc862abd4c96111cc99264e44992eb32132e33
Pablo <pabarragan94@gmail.com>
2023-02-27 23:11:02 +0100

    remove twitter

--- components/bio.tsx
+++ components/bio.tsx
@@ -1,50 +1,55 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram, faGithub, faYoutube, IconDefinition, faTwitter} from '@fortawesome/free-brands-svg-icons'
-import Link from 'next/link'
-import ThemeChanger from './theme-changer'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faGithub,
+  faYoutube,
+  IconDefinition,
+  faTelegram,
+} from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
+import ThemeChanger from "./theme-changer";
 
-
-interface socialItem{
-    url: string,
-    icon: IconDefinition,
-    title: string
+interface socialItem {
+  url: string;
+  icon: IconDefinition;
+  title: string;
 }
 
-const Bio = ()=>{
+const Bio = () => {
+  const social: socialItem[] = [
+    { url: "github.com/pbl0", icon: faGithub, title: "Github" },
+    { url: "t.me/pablobls", icon: faTelegram, title: "Telegram" },
+    {
+      url: "www.youtube.com/channel/UCoI4HY4K4CB_J8O_Mphlnbw",
+      icon: faYoutube,
+      title: "Youtube",
+    },
+  ];
 
-    const social: socialItem[] = [
-        {url:'github.com/pbl0', icon: faGithub, title:'Github'},
-        {url:'twitter.com/hipi_hapa', icon: faTwitter, title:'Twitter'},
-        {url:'www.youtube.com/channel/UCoI4HY4K4CB_J8O_Mphlnbw', icon: faYoutube, title: 'Youtube'}
-    ]
+  let links = [];
 
-    let links = []
-
-    for (let item of social){
-        if (item.url != ''){
-            let link = (
-                <div key={item.url} className='column mr-1 is-2-mobile'>
-                    <a title={item.title} href={`https://${item.url}`}>
-                        <FontAwesomeIcon className='my-icon' icon={item.icon}/>
-                    </a>
-                </div>
-
-            )
-    
-            links.push(link)
-        }
-
-    }
-
-    return (
-        <div className="column my-6 bio">
-            <div className="columns is-mobile is-pulled-right">
-                <ThemeChanger></ThemeChanger>
-                {links}
-            </div>
-            
+  for (let item of social) {
+    if (item.url != "") {
+      let link = (
+        <div key={item.url} className="column mr-1 is-2-mobile">
+          <a title={item.title} href={`https://${item.url}`}>
+            <FontAwesomeIcon className="my-icon" icon={item.icon} />
+          </a>
         </div>
-    )
-}
+      );
 
-export default Bio
+      links.push(link);
+    }
+  }
+
+  return (
+    <div className="column my-6 bio">
+      <div className="columns is-mobile is-pulled-right">
+        <ThemeChanger></ThemeChanger>
+        {links}
+      </div>
+    </div>
+  );
+};
+
+export default Bio;
